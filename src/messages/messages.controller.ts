699e232028f129e93872e90231180728b1bc31be
MatoXD99 <maticpohovski@gmail.com>
2022-10-13 22:02:30 +0200

    Initial commit
    - Initial commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'List of messages';
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+    return body.content;
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+    return 'id: ' + id;
+  }
+}
